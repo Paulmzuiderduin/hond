bffdebe3d733deb71331a6d04ee7b1973a7f52f5
Paulmzuiderduin <130547467+Paulmzuiderduin@users.noreply.github.com>
2024-04-04 14:55:09 +0000

    commit

--- Hello.py
+++ Hello.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
 st.title("Welkom bij de analyseapp van Waterpolo Insight")
 st.text("Voer hier je .CSV bestand in")
@@ -9,13 +10,11 @@ else:
         st.write("Je hebt nog niet op de knop geklikt")
 st.button("Reset", type="primary") 
 
-import streamlit as st
-from streamlit_gsheets import GSheetsConnection
 
 # Create a connection object.
 conn = st.connection("gsheets", type=GSheetsConnection)
-
-df = conn.read()
+url = "https://docs.google.com/spreadsheets/d/1MNbnI5pV7LVZvGWAr9rvy0efqehJWdVEpI_tnw52iJM/edit#gid=0"
+df = conn.read(spreadsheet=url)
 
 # Print results.
 for row in df.itertuples():
